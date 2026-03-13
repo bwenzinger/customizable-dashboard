90ce162b2734a7512d67521dd6078c2c31beff38
bwenzinger <58698973+bwenzinger@users.noreply.github.com>
2026-03-13 15:12:19 -0400

    Update theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { createTheme } from '@mui/material/styles';
 
-const PAGE_PADDING_X = 20;
-const PAGE_PADDING_BOTTOM = 20;
+// const PAGE_PADDING_X = 20;
+// const PAGE_PADDING_BOTTOM = 20;
 const MAX_SHADOW_HORIZONTAL_GUTTER = 20; // this should be adjusted if customStyles -> interactiveCard boxShadow is updated
 const MAX_SHADOW_VERTICAL_GUTTER = 20; // this should be adjusted if customStyles -> interactiveCard boxShadow is updated
 
@@ -32,7 +32,7 @@ theme = createTheme(theme, {
         '0px 1px 2px rgba(16, 24, 40, 0.04),0px 4px 12px rgba(16, 24, 40, 0.06)',
       borderRadius: '12px',
       backgroundColor: '#ffffff',
-      padding: 3,
+      padding: '12px',
       transition:
         'box-shadow 180ms cubic-bezier(0.2, 0, 0, 1),transform 180ms cubic-bezier(0.2, 0, 0, 1)',
     },
@@ -45,13 +45,12 @@ theme = createTheme(theme, {
           '0px 4px 8px rgba(16, 24, 40, 0.06),0px 12px 24px rgba(16, 24, 40, 0.10)',
         cursor: 'pointer',
       },
-    },
-    pagePaddingBottom: {
-      paddingBottom: `${PAGE_PADDING_BOTTOM}px`,
+      boxShadow: '0 2px 4px #0000000f',
     },
     shadowGutter: {
       marginX: `-${MAX_SHADOW_HORIZONTAL_GUTTER}px`,
       paddingX: `${MAX_SHADOW_HORIZONTAL_GUTTER}px`,
+      paddingBottom: `${MAX_SHADOW_VERTICAL_GUTTER}px`,
     },
   },
 });
